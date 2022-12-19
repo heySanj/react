9bdae7ec89d76cd12df23564263e1940f014b0e1
heySanj <saprasad92@gmail.com>
2022-12-19 12:24:59 +1000

    Learning useEffect

---
+++ effects/src/components/Login/Login.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+
+  // ==================================================================================================
+
+  useEffect(() => {
+
+    // Only execute this function after a 500ms delay
+    // IF the user types anything else, this timer will be cleared due to the cleanup function
+    // and the timer will restart; useful for limiting the frequency of function calls
+    const timerID = setTimeout(()=>{
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500)
+
+    // Returning from useEffect has to be a function --> a cleanup function
+    // The cleanup function actually runs BEFORE the rest of the useEffect function (except for the FIRST time)
+    // The cleanup function also runs when this element is removed from the DOM
+    return () => {
+      clearTimeout(timerID) // Clear the last timer if it has not yet executed
+    }
+
+  }, [ enteredEmail, enteredPassword ]) // Only run this effect if ANY of these dependancies change
+
+  // ==================================================================================================
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
