89d6011fe250f96d55dca204a3a5fd640d10d314
heySanj <saprasad92@gmail.com>
2023-02-01 22:34:17 +1000

    Learning useCallback

--- revision-12/src/App.js
+++ revision-12/src/App.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button'
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
 
   const [showParagraph, setShowParagraph] = useState(false)
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  }
+  }, [])
   
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
