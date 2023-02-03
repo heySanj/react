95a3607806cf6109d910e9c7d3de24b2b0244835
heySanj <saprasad92@gmail.com>
2023-02-03 14:03:08 +1000

    Revised states, useCallback and useMemo

--- revision-12/src/App.js
+++ revision-12/src/App.js
@@ -7,16 +7,23 @@ import './App.css';
 function App() {
 
   const [showParagraph, setShowParagraph] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false);
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
-  }, [])
+    if(allowToggle){
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+    }
+  }, [allowToggle])
   
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
